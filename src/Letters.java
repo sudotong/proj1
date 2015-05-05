98274345cd317f63a9da0c2296bbb726b3589f56
Samuel Udotong <sudotong@gmail.com>
2015-05-05 17:22:49 -0400

    Adding letters A and V

--- src/Letters.java
+++ src/Letters.java
@@ -34,18 +34,27 @@ public class Letters {
 		//			System.exit(0);
 		//		}
 		ArrayList<Instruction> alist = new ArrayList<Instruction>();
+		Instruction leftLine = null;
+		Instruction rightLine = null;
+		Instruction centerLine = null;
+		
 		switch(letter){
 		case 'A':
-			double xstart = 1;
-			double ystart = 2;
+			double xleft_startA = this.xBoxSize*1/4;
+			double xright_startA = this.xBoxSize*3/4;
+			double ystartA = 0;
+			double xtopA = this.xBoxSize/2;
+			double ytopA = this.yBoxSize;
+			double ymiddleA = this.yBoxSize/2;
+			double xmiddle_startA = this.getPointInSlope(xleft_startA,ystartA,xtopA,ytopA,ymiddleA,"x");
+			double xmiddle_endA = this.getPointInSlope(xright_startA,ystartA,xtopA,ytopA,ymiddleA,"x");					
 
-			Instruction cornerLine = new Instruction(Instruction.LINE,new double[]{xstart,ystart},new double[]{xstart,ystart},0);
-
-			Instruction rightline = new Instruction(Instruction.LINE,new double[]{xstart,ystart},new double[]{xstart,ystart},0);
-			Instruction centerline = new Instruction(Instruction.LINE,new double[]{xstart,ystart},new double[]{xstart,ystart},0);
-			alist.add(cornerLine);
-			alist.add(rightline);
-			alist.add(centerline);
+			leftLine = new Instruction(Instruction.LINE,new double[]{xleft_startA,ystartA},new double[]{xtopA,ytopA},0);
+			rightLine = new Instruction(Instruction.LINE,new double[]{xright_startA,ystartA},new double[]{xtopA,ytopA},0);
+			centerLine = new Instruction(Instruction.LINE,new double[]{xmiddle_startA,ymiddleA},new double[]{xmiddle_endA,ymiddleA},0);
+			alist.add(leftLine);
+			alist.add(rightLine);
+			alist.add(centerLine);
 			break;
 		case 'B':
 			break;
@@ -53,10 +62,35 @@ public class Letters {
 			break;
 		case 'D':
 			break;
-
+		case 'V':
+			double xleft_startV = this.xBoxSize*1/4;
+			double xright_startV = this.xBoxSize*3/4;
+			double ystartV = this.yBoxSize;
+			double xbottomV = this.xBoxSize/2;
+			double ybottomV = 0;
+			leftLine = new Instruction(Instruction.LINE,new double[]{xleft_startV,ystartV}, new double[]{xbottomV,ybottomV},0);
+			rightLine = new Instruction(Instruction.LINE,new double[]{xright_startV,ystartV}, new double[]{xbottomV,ybottomV},0);
+			alist.add(leftLine);
+			alist.add(rightLine);
+			break;
+			
 		default:
 			break;
 		}
 		return alist;
+	}
+	
+	private double getPointInSlope(double xstart,double ystart,double xend,double yend,double intercept,String type){
+		double point;
+		double slope = (yend-ystart)/(xend-xstart);
+		double b = ystart - slope*xstart;
+		if (type.equals("x")){ // want to get x value at intercept
+			point = (intercept - b)/slope;
+		} else if (type.equals("y")){ // want to get y value at intercept
+			point = slope*intercept + b; 
+		} else {
+			return Double.NEGATIVE_INFINITY;
+		}		
+		return point;
 	}
 }
